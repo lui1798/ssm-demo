a774c3017005c7d0d114b1869dd69a7cbfc35067
Administrator <lui1798@163.com>
2019-08-11 12:30:04 +0800

    springmvc基本框架

--- src/main/java/cn/jp51/ssm/web/UserController.java
+++ src/main/java/cn/jp51/ssm/web/UserController.java
@@ -6,7 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -17,12 +20,18 @@ public class UserController {
     private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    UserService userService;
+    private UserService userService;
 
     @RequestMapping("/test")
-    public String list(int offset, int limit) {
+    public String list(Model model) {
+        LOG.info("invoke----------/user/list");
         List<User> userList = userService.getUserList(1, 10);
-        return "userList";
+        System.out.println(userList);
+//        ModelAndView view = new ModelAndView();
+//        view.addObject("userList", userList);
+//        view.setViewName("userList");
+        model.addAttribute("userlist", userList);
+        return "userlist";
     }
 
 }
